3828048dec03afdd7de6abe3d3f55b0541b1ab35
yinasaurus <shiyin.li.2024@smu.edu.sg>
2025-10-28 07:16:10 +0000

    updated ex4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,43 +1,56 @@
 <script>
-    export default {
-        data() {
-            return {
-                desc: '',
-                deadline: '',
-                taskList: []
-            }
-        },
-        methods: {
-            add() {
-                this.taskList.push( { 'desc': this.desc, 'deadline': this.deadline } )
-                this.desc = ''
-                this.deadline = ''
-            },
-            // TODO: Add a new method, to delete a task completed
-            
-        }
-    }
+import TaskTracker from './subcomponents/TaskTracker.vue'
 
+export default {
+  components: { TaskTracker },
+  data() {
+    return {
+      desc: '',
+      deadline: '',
+      taskList: []
+    }
+  },
+  methods: {
+    add() {
+      if (!this.desc || !this.deadline) return
+
+      this.taskList.push({
+        desc: this.desc,
+        deadline: this.deadline
+      })
+
+      this.desc = ''
+      this.deadline = ''
+    },
+    removeTask(idx) {
+      this.taskList.splice(idx, 1)
+    }
+  }
+}
 </script>
 
 <template>
+  <div class="container mt-3">
     <div class="mb-3">
-        <label for="desc" class="form-label">Task</label>
-        <input type="text" class="form-control" id="desc" v-model='desc' placeholder="task">
-    </div>
-    <div class="mb-3">
-        <label for="deadline" class="form-label">Deadline</label>
-        <input type="date" class="form-control" id="deadline" v-model='deadline' placeholder="deadline">
+      <label for="desc" class="form-label">Task</label>
+      <input type="text" class="form-control" id="desc" v-model="desc" placeholder="task">
     </div>
 
-    <button type="button" @click="add" class="btn btn-primary">Add New Task</button>
+    <div class="mb-3">
+      <label for="deadline" class="form-label">Deadline</label>
+      <input type="date" class="form-control" id="deadline" v-model="deadline" placeholder="deadline">
+    </div>
+
+    <button type="button" class="btn btn-primary" @click="add">Add New Task</button>
     <hr>
 
-    <!-- TODO: Modify following code -->
-    <task-tracker ></task-tracker>
-
+    <!-- Display tasks using TaskTracker -->
+    <TaskTracker 
+      v-for="(task, index) in taskList"
+      :key="index"
+      :task="task"
+      :idx="index"
+      @done="removeTask"
+    />
+  </div>
 </template>
-
-<style scoped>
-   
-</style>
--- src/components/subcomponents/TaskTracker.vue
+++ src/components/subcomponents/TaskTracker.vue
@@ -1,18 +1,25 @@
 <script>
-    // TODO: Complete the code
-    export default { 
-        data() {
-            return {
-                
-            }
-        },
-        // Hint: insert properties task (Object) and idx (Number)
+export default {
+  props: {
+    task: Object,
+    idx: Number
+  },
+  methods: {
+    markDone() {
+      this.$emit('done', this.idx)
     }
+  }
+}
 </script>
 
 <template>
-    <!-- TODO: add your template code here --> 
-   
+  <div class="card mb-2">
+    <div class="card-body d-flex justify-content-between align-items-center">
+      <div>
+        <h5 class="card-title">Deadline: {{ task.deadline }}</h5>
+        <p class="card-text">{{task.desc}}</p>
+      </div>
+      <button class="btn btn-success" @click="markDone">Done</button>
+    </div>
+  </div>
 </template>
-
-
