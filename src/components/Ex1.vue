06694a9b53eb21d4baaf0290c7e9d34f1a1659c3
yinasaurus <shiyin.li.2024@smu.edu.sg>
2025-10-28 06:21:45 +0000

    updated ex1 and 2

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,13 +1,18 @@
 <script>
+import { computed } from 'vue';
+import SoundIcon from './subcomponents/SoundIcon.vue'
+
     //import component
     
     export default { 
+        components:{SoundIcon},
         data() {
             return {
                 soundLevel: 0
             }
         }
     }
+    
 </script>
 
 <template>
@@ -15,7 +20,7 @@
    Sound level
     <input type="number" min="0" max="3" v-model="soundLevel"> 
     <!-- TODO: Add Code Here to make use of the component -->
-    
+    <sound-icon :level="soundLevel"></sound-icon>
 
 </template>
 
--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,39 +1,37 @@
 <script>
-    // Import BlogPost component
-    import blogPost from './subcomponents/BlogPost.vue'
-	import axios from 'axios'
-    export default {
-        data() {
-            return {
-                posts: [] // array of post objects
-            }  
-        },
-        computed: {
-            baseUrl() {
-                if (window.location.hostname=='localhost')
-                    return 'http://localhost:3000' 
-                else {
-                    const codespace_host = window.location.hostname.replace('5173', '3000')
-                    return `https://${codespace_host}`;
-                }
-            }
-        },
-        created() { // created is a hook that executes as soon as Vue instance is created
-            axios.get(`${this.baseUrl}/posts`)
-            .then(response => {
-                // this gets the data, which is an array
-                this.posts = response.data
-                console.log(response.data)
-            })
-            .catch(error => {
-                this.posts = [{ entry: 'There was an error: ' + error.message }]
-            })
-        }
+import axios from 'axios'
+import BlogPost from './subcomponents/BlogPost.vue'
+
+export default {
+  components: { BlogPost },
+  data() {
+    return {
+      posts: [] // array of post objects
+    }  
+  },
+  computed: {
+    baseUrl() {
+      if (window.location.hostname === 'localhost')
+        return 'http://localhost:3000'
+      else {
+        const codespace_host = window.location.hostname.replace('5173', '3000')
+        return `https://${codespace_host}`
+      }
     }
+  },
+  created() { 
+    axios.get(`${this.baseUrl}/posts`)
+      .then(response => {
+        this.posts = response.data
+      })
+      .catch(error => {
+        this.posts = [{ subject: 'Error', entry: 'There was an error: ' + error.message, mood: 'error' }]
+      })
+  }
+}
 </script>
 
 <template>
-   <!-- TODO: make use of the 'blog-post' component to display the blog posts -->
-
+    <blog-post v-for="post in posts" :subject="post.subject" 
+    :entry="post.entry" :mood="post.mood" :key="post.id"></blog-post>
 </template>
-
